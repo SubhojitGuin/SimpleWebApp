dc4b0a4300714d2b979d05e2b01c18fb5e3f1916
Subhojit Guin <subhojitguin2004@gmail.com>
2026-03-11 14:37:44 +0530

    Initial commit

---
+++ src/main/java/com/example/webapp/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.example.webapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<h2>Hello from Maven Web Application!</h2>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Maven WebApp</title>
+</head>
+
+<body>
+
+<h1>Welcome to Maven Web Application</h1>
+
+<a href="hello">Click to call Servlet</a>
+
+</body>
+</html>
